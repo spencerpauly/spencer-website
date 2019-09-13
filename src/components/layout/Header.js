eb3359be9ec295cfdf8b689e7f4e98d35e0ab28c
Spencer Pauly <spencerpauly@gmail.com>
2019-09-12 19:31:13 -0500

    Changed main header layout - like the new layout so this will stay in master

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import "./Header.css"
+import Button from "react-bootstrap/Button";
 
 function Header() {
     return (
         <header className='header'>
-            <img src="./images/avatar-1.jpg" alt="header"/>
-            <h1>Spencer Pauly</h1>
-            <p>Computer Science Student</p>
-            <p>University of Minnesota Duluth</p>
+            <div>
+                <img src="./images/avatar-1.jpg" alt="header"/>
+            </div>
+            <div>
+                <h1>Spencer Pauly</h1>
+                <p>I'm a Computer Science B.S. Student at University of Minnesota Duluth. I'm currently looking for an internship for the summer of 2020 in the field of software engineering.</p>
+                <p>If you are a company looking to hire or just a fellow programmer who would like to discuss stuff. Feel free to...</p>
+                <Button variant="danger">Contact Me</Button>
+            </div>
+            
+            
         </header>
     )
 }
