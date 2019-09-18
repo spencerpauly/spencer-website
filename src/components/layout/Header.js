ed728206d9de0299d145edfe00c0b472d7a9d3a2
Spencer Pauly <spencerpauly@gmail.com>
2019-09-18 16:15:07 -0500

    Began fixing issues with responsiveness discovered at smaller screen sizes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,6 +8,7 @@ function Header() {
             <div>
                 <img src="./images/avatar-1.jpg" alt="header"/>
             </div>
+
             <div>
                 <h1>Spencer Pauly</h1>
                 <p>I'm a Computer Science B.S. Student at University of Minnesota Duluth. I'm currently looking for an internship for the summer of 2020 in the field of software engineering. My goals are to develop awesome software in a modern environment.</p>
--- src/components/layout/NavigationBar.js
+++ src/components/layout/NavigationBar.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react'
 import NavLogo from './NavLogo'
 import NavItem from './NavItem'
 import "./NavigationBar.css"
-import MediaQuery from 'react-responsive'
-
-
 
 export class NavigationBar extends Component {
 
@@ -15,13 +12,13 @@ export class NavigationBar extends Component {
                     <NavLogo image="./images/logo-secondary.png" link="/" />
                     <NavItem name="spencerpauly.com" link="/" />
                 </div>
-                <MediaQuery minDeviceWidth={1024}>
+                <div>
                     <div className="navbar-right">
                         <NavItem name="About Me" link="/about" />
                         <NavItem name="My Projects" link="/projects" />
                         <NavItem name="Contact Me" link="/contact" />
                     </div>
-                </MediaQuery>
+                </div>
                 
             </div>
         )
